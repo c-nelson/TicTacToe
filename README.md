97ae7640b3b3a6e96ed25a410e011f53a231d583
c-nelson <nelson.christian.m@gmail.com>
2017-08-17 19:20:56 -0400

    Added all clion files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TicTacToe
 TicTacToe game made for CS161. Made in August 2017.
+Made in C Lion
---
+++ cmake-build-debug/CMakeFiles/clion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion.dir/TicTacToe.cpp.o"
+  "CMakeFiles/clion.dir/Board.cpp.o"
+  "clion.pdb"
+  "clion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
